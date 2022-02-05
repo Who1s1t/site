2d2c2003a9fd2d88b1ef5a81854a27c6678f6523
OVER <dedsek965@gmail.com>
2022-02-05 20:57:45 +0300

    add def

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def search():
+    return HttpResponse('Search :')
+def ribbon():
+    return HttpResponse("Ribbon")
