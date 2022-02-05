e1a61a0f71e8edf8a7436c08af86d2a5d75f5967
OVER <dedsek965@gmail.com>
2022-02-05 21:08:38 +0300

    add url

---
+++ mysite/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from blog.views import search, ribbon, home
+urlpatterns = [
+    path('', home),
+    path('search/', search),
+    path('ribbon/', ribbon)
+]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #TODO Сюда добавляем дополнительные ветки
     path('', include('news.urls')),
+    path('', include('blog.urls'))
 ]
