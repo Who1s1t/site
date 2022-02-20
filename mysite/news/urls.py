615e225a47db2fb72724ed5c2e4dc99b83eb715c
OVER <dedsek965@gmail.com>
2022-02-21 02:11:26 +0300

    add archive

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from news.views import test,test_new
-#TODO создаем файлик urls.py для разветвления
+from news.views import test, archive, archiveset
+from news.models import MyModel
+
+# TODO создаем файлик urls.py для разветвления
 # Указываем:
 urlpatterns = [
     path('', test),
-    path('test/', test_new),
+    path('archive/', archive)
 ]
+for i in [i.strftime("%Y-%m-%d") for i in MyModel.objects.dates('created_at', "day", order='ASC')]:
+    urlpatterns.append(path(f'archive/{i}/', archiveset))
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,20 +1,40 @@
 from django.http import HttpResponse
+import datetime
 from news.models import MyModel
 from django.shortcuts import render
+
 
 # Create your views here.
 
 def test(request):
     print(request)
     news = MyModel.objects.all()
-    #res = "<h1>Новости</h1>"
-    #for i in news:
+    # res = "<h1>Новости</h1>"
+    # for i in news:
     #   res += f"\n<p>{i.caption}</p>\n<p>{i.text}</p>\n<p>{i.created_at}</p>\n<hr>"
     context = {
         "news": news,
-        "title":"Список новостей"
+        "title": "Список новостей"
     }
-    return render(request,"news/test.html",context)
+    return render(request, "news/test.html", context)
 
-def test_new(request):
-    return HttpResponse('Some text')
+
+def archive(request):
+    news = [i.strftime("%Y-%m-%d") for i in MyModel.objects.dates('created_at', "day", order='ASC')]
+    context = {
+        "news": news,
+        "title": "Список новостей"
+    }
+    return render(request, "news/archive.html", context)
+
+
+def archiveset(request):
+    date = list(map(int,request.get_full_path()[9:-1].split('-')))
+    print(date)
+    news = MyModel.objects.filter(created_at__date=datetime.date(date[0], date[1], date[2]))
+    print(news)
+    context = {
+        "news": news,
+        "title": "Список новостей"
+    }
+    return render(request, "news/test.html", context)
