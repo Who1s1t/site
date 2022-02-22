dc0449f274e9e9877f28d92210a8e1910d74e5b9
OVER <dedsek965@gmail.com>
2022-02-22 17:52:38 +0300

    Edit

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from news.models import MyModel, Category
 # Register your models here.
+class MyModel_Admin(admin.ModelAdmin):
+    list_display = ('id', 'caption', "created_at", "updated_at", "is_published")
+    list_display_links = ('id', 'caption')
+    search_fields = ('caption', 'text')
+admin.site.register(MyModel,MyModel_Admin)
+admin.site.register(Category)
+
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -3,12 +3,20 @@ from django.db import models
 
 class MyModel(models.Model):
     # id = models.BigIntegerField()
-    caption = models.CharField(max_length=100)
+    caption = models.CharField(max_length=100,verbose_name="Заголовок")
     text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to="media/", height_field=500, width_field=500)
-    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновленно")
+    photo = models.ImageField(upload_to="media/%Y/%m/%d", blank=True)
+    is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
 
     def __str__(self):
         return self.caption
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+        ordering = ['id']
+
+class Category(models.Model):
+    category = models.CharField(max_length=100, db_index=True)
