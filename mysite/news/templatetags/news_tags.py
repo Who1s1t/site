f0de50f95f2b1c228d36a164a682ca352200e943
OVER <dedsek965@gmail.com>
2022-05-10 04:49:08 +0300

    task 1

--- mysite/news/templatetags/news_tags.py
+++ mysite/news/templatetags/news_tags.py
@@ -1,5 +1,5 @@
 from django import template
-
+from django.db.models import Count
 from news.models import Category
 
 register = template.Library()
@@ -7,4 +7,4 @@ register = template.Library()
 
 @register.simple_tag()
 def categories_get():
-    return Category.objects.all()
+    return Category.objects.annotate(category__count=Count('news'))
