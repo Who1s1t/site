2bfa1712fec24d8bbead54324d7f32d3987249c4
OVER <dedsek965@gmail.com>
2022-05-24 00:35:47 +0300

    edit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,8 +17,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-
+import debug_toolbar
 urlpatterns = [
+    path('__debug__/', include(debug_toolbar.urls)),
     path('admin/', admin.site.urls),
     # TODO Сюда добавляем дополнительные ветки
     path('', include('news.urls'))
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -15,7 +15,7 @@ class ListNews(ListView):
     model = News
     template_name = "news/news_list.html"
     context_object_name = "news"
-
+    queryset = News.objects.filter(is_published=True).select_related('category')
     # extra_context = {"title":"Новости","categories":ct_get()}
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
